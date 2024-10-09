da8fd48af83151ff3ce5cdf4b84baef604d6cd49
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2024-10-09 19:39:37 +0600

    changed db to prod and dev mod

--- config/db.js
+++ config/db.js
@@ -1,9 +1,20 @@
 const {Sequelize} = require('sequelize')
 const dbConf = require('./config')
-const sequelize = new Sequelize(dbConf.development.database, dbConf.development.username, dbConf.development.password, {
-    host: dbConf.development.host,
-    dialect: dbConf.development.dialect
-})
+
+let sequelize;
+if(process.env === "production"){
+    sequelize = new Sequelize(dbConf.production.database, dbConf.production.username, dbConf.production.password, {
+        host: dbConf.production.host,
+        dialect: dbConf.production.dialect,
+        port: dbConf.production.port
+    })
+}else {
+    sequelize = new Sequelize(dbConf.development.database, dbConf.development.username, dbConf.development.password, {
+        host: dbConf.development.host,
+        dialect: dbConf.development.dialect
+    })
+}
+
 
 sequelize
 .authenticate()
