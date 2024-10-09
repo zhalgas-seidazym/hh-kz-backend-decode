30f3b29e65755bf8b2492855b404399bc3635ff0
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2024-10-09 20:05:55 +0600

    added ssl reader for prod db conf

--- config/db.js
+++ config/db.js
@@ -1,12 +1,19 @@
 const {Sequelize} = require('sequelize')
-const dbConf = require('./config')
+const fs = require('fs')
+
+const dbConf = require('./config');
 
 let sequelize;
 if(process.env.NODE_ENV === "production"){
     sequelize = new Sequelize(dbConf.production.database, dbConf.production.username, dbConf.production.password, {
         host: dbConf.production.host,
         dialect: dbConf.production.dialect,
-        port: dbConf.production.port
+        port: dbConf.production.port,
+        dialectOptions: {
+            ssl: {
+                ca: fs.readFileSync('/config/ca-certificate.crt')
+            }
+        }
     })
 }else {
     sequelize = new Sequelize(dbConf.development.database, dbConf.development.username, dbConf.development.password, {
